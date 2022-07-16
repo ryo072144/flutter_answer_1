460f911c8a958de125a93a68baa0f92a6f34b6b4
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-16 19:25:27 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -15,85 +17,87 @@ class MyApp extends StatelessWidget {
         //問１： ここでは基本色しか指定できません。
         primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('タイトル', style: TextStyle(fontSize: 20, color: Color(0xff222222), fontWeight: FontWeight.bold),),),
+        appBar: AppBar(title: Text('タイトル', style: const TextStyle(fontSize: 20, color: Color(0xff222222), fontWeight: FontWeight.bold),),),
 
-      //問２： 真黒(0xff000000)や真白(0xffffffff)は目が疲れるので、あまり使わないようにしましょう。
-      //Flutterのカラーコードは8桁ですが、頭の2桁は透過率を表します。
-      backgroundColor: Color(0xffeeeeee),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-            child: Row(
+        //問２： 真黒(0xff000000)や真白(0xffffffff)は目が疲れるので、あまり使わないようにしましょう。
+        //Flutterのカラーコードは8桁ですが、頭の2桁は透過率を表します。
+        backgroundColor: const Color(0xffeeeeee),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+              child: Row(
 
-              //問７： mainAxisAlignmentでColumnやRowと平行に位置を調節できます。
-              //crossAxisAlignmentはColumnやRowと直角に位置を調整できます。
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
-                SizedBox(width: 10,),
+                //問７： mainAxisAlignmentでColumnやRowと平行に位置を調節できます。
+                //crossAxisAlignmentはColumnやRowと直角に位置を調整できます。
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
+                  SizedBox(width: 10,),
 
-                //問４: アイコンの一覧はこちらで確認できます(https://api.flutter.dev/flutter/material/Icons-class.html)。
-                //SizedBoxはちょっとした隙間を作りたいときに便利です。
-                Icon(Icons.thumb_down, color: Colors.red, size: 30,),
-                SizedBox(width: 10,),
-                Text('タイル１', style: TextStyle(fontSize: 18, color: Color(0xff222222),),)
-              ],
+                  //問４: アイコンの一覧はこちらで確認できます(https://api.flutter.dev/flutter/material/Icons-class.html)。
+                  //SizedBoxはちょっとした隙間を作りたいときに便利です。
+                  Icon(Icons.thumb_down, color: Colors.red, size: 30,),
+                  SizedBox(width: 10,),
+                  Text('タイル１', style: TextStyle(fontSize: 18, color: Color(0xff222222),),)
+                ],
+              ),
             ),
-          ),
-          Divider(height: 20,),
-          Container(
+            const Divider(height: 20,),
+            Container(
 
-            //問５： paddingはWidgetの大きさを広げるプロパティです。
-            padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
-                SizedBox(width: 10,),
-                Text('タイル2', style: TextStyle(fontSize: 18, color: Color(0xff222222),))
-              ],
+              //問５： paddingはWidgetの大きさを広げるプロパティです。
+              padding: const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
+                  SizedBox(width: 10,),
+                  Text('タイル2', style: TextStyle(fontSize: 18, color: Color(0xff222222),))
+                ],
+              ),
             ),
-          ),
-          Divider(height: 20,),
-          Container(
-            padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
-                SizedBox(width: 10,),
+            const Divider(height: 20,),
+            Container(
+              padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
+                  SizedBox(width: 10,),
 
-                //問６： fontWeightで文字の太さを調整できます。
-                Text('タイル３', style: TextStyle(fontSize: 18, color: Color(0xff222222), fontWeight: FontWeight.bold))
-              ],
+                  //問６： fontWeightで文字の太さを調整できます。
+                  Text('タイル３', style: TextStyle(fontSize: 18, color: Color(0xff222222), fontWeight: FontWeight.bold))
+                ],
+              ),
             ),
-          ),
-          Divider(height: 20,),
+            const Divider(height: 20,),
 
-          //問３： どの部分がタイルなのかが分かれば大丈夫です。
-          Container(
-            padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-            child: Row(
-              children: [
-                Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
-                SizedBox(width: 10,),
-                Text('タイル４', style: TextStyle(fontSize: 18, color: Color(0xff222222),))
-              ],
+            //問３： Dividerで分けた部分をタイルとしています。
+            Container(
+              padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+              child: Row(
+                children: const [
+                  Icon(Icons.thumb_up, color: Colors.blue, size: 30,),
+                  SizedBox(width: 10,),
+                  Text('タイル４', style: TextStyle(fontSize: 18, color: Color(0xff222222),))
+                ],
+              ),
             ),
-          ),
-          Divider(height: 20,),
-        ],
-      )
+            const Divider(height: 20,),
+          ],
+        )
     );
   }
 }
